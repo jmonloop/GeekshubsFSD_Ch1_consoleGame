76f856a1c02131c7ed571048570820333d5997d3
jmonloop <monleonlopezjavier@gmail.com>
2022-01-20 19:30:01 +0100

    transition forwards (keep) added

--- js/main.js
+++ js/main.js
@@ -10,3 +10,12 @@ function moveCenter() {
     document.getElementById('_left').classList.toggle('leftMove');
     document.getElementById('_right').classList.toggle('rightMove');
 }
+
+let switchedOn = false;
+function switchOnOff() {
+    if(switchedOn === false) {
+        document.getElementById('_onOffswitch').classList.toggle('switchOn');
+        switchedOn = true;
+    }
+    
+}
